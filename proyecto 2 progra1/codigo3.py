c48e218619ce55b8b25e1791b8b654469fcde943
brunorojas131 <bruno.rojas2024@alu.uct.cl>
2024-06-09 05:17:15 -0400

    versión casi si es que no definitiva

--- proyecto 2 progra1/codigo3.py
+++ proyecto 2 progra1/codigo3.py
@@ -11,7 +11,7 @@ database_path = "./Sql_data/db_personas.db"
 conn = sqlite3.connect("db_personas.db")
 query = """
 SELECT p.nombre_completo AS nombre, p.nacionalidad, s.Rol AS rol, s.Sueldo AS salario
-FROM personas 
+FROM personas p
 INNER JOIN Salarios s ON p.id_rol = s.id_salarios
 """
 df = pd.read_sql_query(query, conn)
